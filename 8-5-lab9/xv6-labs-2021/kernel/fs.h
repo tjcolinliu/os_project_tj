6f50d77d3bc26d319a5924236cf61cc0e2cf11fa
tjcolinliu <2478167310@qq.com>
2023-08-05 17:12:55 +0800

    最后冲刺！

---
+++ 8-5-lab9/xv6-labs-2021/kernel/fs.h
@@ -0,0 +1,67 @@
+// On-disk file system format.
+// Both the kernel and user programs use this header file.
+
+
+#define ROOTINO  1   // root i-number
+#define BSIZE 1024  // block size
+
+// Disk layout:
+// [ boot block | super block | log | inode blocks |
+//                                          free bit map | data blocks]
+//
+// mkfs computes the super block and builds an initial file system. The
+// super block describes the disk layout:
+struct superblock {
+  uint magic;        // Must be FSMAGIC
+  uint size;         // Size of file system image (blocks)
+  uint nblocks;      // Number of data blocks
+  uint ninodes;      // Number of inodes.
+  uint nlog;         // Number of log blocks
+  uint logstart;     // Block number of first log block
+  uint inodestart;   // Block number of first inode block
+  uint bmapstart;    // Block number of first free map block
+};
+
+#define FSMAGIC 0x10203040
+
+//该常量定义了原先直接索引的数量
+#define NDIRECT 12
+//该常量定义了一个索引结点中最多能有多少索引
+#define NINDIRECT (BSIZE / sizeof(uint))
+//该常量定义了整个文件最大由多少数据块构成，NDIRECT -1代表直接索引，NINDIRECT代表二级索引对应的索引数，NINDIRECT * NINDIRECT代表三级索引对应的索引数
+#define MAXFILE (NDIRECT -1 + NINDIRECT + NINDIRECT * NINDIRECT)
+
+#define FIRSTINDEX 11
+#define SECONDINDEX (BSIZE / sizeof(uint))
+#define THIRDINDEX (SECONDINDEX*SECONDINDEX)
+
+// On-disk inode structure
+struct dinode {
+  short type;           // File type
+  short major;          // Major device number (T_DEVICE only)
+  short minor;          // Minor device number (T_DEVICE only)
+  short nlink;          // Number of links to inode in file system
+  uint size;            // Size of file (bytes)
+  uint addrs[NDIRECT+1];   // Data block addresses
+};
+
+// Inodes per block.
+#define IPB           (BSIZE / sizeof(struct dinode))
+
+// Block containing inode i
+#define IBLOCK(i, sb)     ((i) / IPB + sb.inodestart)
+
+// Bitmap bits per block
+#define BPB           (BSIZE*8)
+
+// Block of free map containing bit for block b
+#define BBLOCK(b, sb) ((b)/BPB + sb.bmapstart)
+
+// Directory is a file containing a sequence of dirent structures.
+#define DIRSIZ 14
+
+struct dirent {
+  ushort inum;
+  char name[DIRSIZ];
+};
+
